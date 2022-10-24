0a99bf8f9c06cefa5d14c63e689091e4841a883c
Ayyanaruto <ayyan.shaikh.narotu@gmail.com>
2022-10-24 18:32:44 +0530

    commit suicide

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -30,7 +30,7 @@ class StreamShow extends React.Component{
         }
         this.player=flv.createPlayer({
             type:'flv',
-            url:url
+            url:`http://localhost:8000/live/${id}.flv`
         })
         this.player.attachMediaElement(this.videoRef.current)
         this.player.load()
